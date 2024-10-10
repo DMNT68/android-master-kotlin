d050a928fb0eb5757848c1666644923c2dcbcb2b
Andrés Salgado <andressalgado41@gmail.com>
2024-10-10 17:30:00 -0500

    Add first app activity

---
+++ app/src/main/java/com/andres_sagadoc/androidmaster/FirstApp/FirstAppActivity.kt
@@ -0,0 +1,17 @@
+package com.andres_sagadoc.androidmaster.FirstApp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.andres_sagadoc.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+
+    }
+}
