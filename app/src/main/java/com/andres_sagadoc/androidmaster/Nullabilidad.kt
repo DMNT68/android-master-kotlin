20a2e0d228a943cd73de16e4cbf55247c2c1fbc3
Andrés Salgado <andressalgado41@gmail.com>
2024-10-10 11:33:04 -0500

    Add cases nullability

---
+++ app/src/main/java/com/andres_sagadoc/androidmaster/Nullabilidad.kt
@@ -0,0 +1,16 @@
+package com.andres_sagadoc.androidmaster
+
+fun main() {
+    var name: String? = "Andrés"
+    var name2: String? = null
+
+    println(name!![3])
+    println(name.get(3))
+    println(name2?.get(3) ?: "Es nulo")
+
+    if (name != null) {
+        if (name.lowercase() == "andres") {
+            println("Es andres")
+        }
+    }
+}
