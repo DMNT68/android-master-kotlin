a772439cc791df4a3234c5b0892092d4ef307271
Andrés Salgado <andressalgado41@gmail.com>
2024-10-14 17:53:45 -0500

    Add logic to first app

--- app/src/main/java/com/andres_sagadoc/androidmaster/FirstApp/FirstAppActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/FirstApp/FirstAppActivity.kt
@@ -1,17 +1,30 @@
 package com.andres_sagadoc.androidmaster.FirstApp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.andres_sagadoc.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
 
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+        }
     }
 }
