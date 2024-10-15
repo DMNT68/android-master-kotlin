73c4978add421fe075aec3a4a6394093eafde389
Andrés Salgado <andressalgado41@gmail.com>
2024-10-15 10:05:12 -0500

    add Exercise5.kt

---
+++ app/src/main/java/com/andres_sagadoc/androidmaster/exercises/Exercise5.kt
@@ -0,0 +1,41 @@
+package com.andres_sagadoc.androidmaster.exercises
+
+/**
+ * 5. Perfil de Internet
+ * A menudo, debes completar los perfiles de sitios web en línea que contienen campos obligatorios
+ * y no obligatorios. Por ejemplo, puedes agregar tu información personal y un vínculo a otras
+ * personas que te refirieron para que registraras tu perfil.
+ *
+ * En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa
+ * que imprima los detalles del perfil de una persona.
+ *
+ *
+ * Completa la función showProfile() para que el programa imprima estas líneas:
+ *
+ *
+ * Name: Amanda
+ * Age: 33
+ * Likes to play tennis. Doesn't have a referrer.
+ *
+ * Name: Atiqah
+ * Age: 28
+ * Likes to climb. Has a referrer named Amanda, who likes to play tennis.
+ */
+
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        val referrer =
+            if (referrer == null) " Doesn't have a referrer" else "Has a referrer named ${referrer.name}, who likes to ${referrer.hobby}"
+        println("Name: $name")
+        println("Age: $age")
+        println("Like to $hobby, $referrer \n")
+    }
+}
