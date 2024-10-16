46f0c72070b6d44f18729dc44e05ace34aff7bd0
Andrés Salgado <andressalgado41@gmail.com>
2024-10-16 12:17:22 -0500

    Remove code unused

--- app/src/main/java/com/andres_sagadoc/androidmaster/imccalculator/IMCActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/imccalculator/IMCActivity.kt
@@ -2,10 +2,8 @@ package com.andres_sagadoc.androidmaster.imccalculator
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
