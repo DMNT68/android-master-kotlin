cd56ed40323eb8959f1ebf5a177ae54fba53d722
Andrés Salgado <andressalgado41@gmail.com>
2024-10-16 16:48:30 -0500

    Update id

--- app/src/main/java/com/andres_sagadoc/androidmaster/FirstApp/ResultActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/FirstApp/ResultActivity.kt
@@ -2,10 +2,7 @@ package com.andres_sagadoc.androidmaster.FirstApp
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.andres_sagadoc.androidmaster.R
 
 class ResultActivity : AppCompatActivity() {
@@ -14,7 +11,7 @@ class ResultActivity : AppCompatActivity() {
         // enableEdgeToEdge()
         setContentView(R.layout.activity_result)
 
-        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val tvResult = findViewById<TextView>(R.id.tvTitleResult)
         val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
         tvResult.text = "Hola $name"
     }
