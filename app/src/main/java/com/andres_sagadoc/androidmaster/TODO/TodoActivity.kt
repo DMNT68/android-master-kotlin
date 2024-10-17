a219687aee11fc272b029ee95326889a32dac643
Andrés Salgado <andressalgado41@gmail.com>
2024-10-17 17:20:01 -0500

    add enableEdgeToEdge and remove changeNameActionBar and enableDarkModeActionBar

--- app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TodoActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TodoActivity.kt
@@ -12,27 +12,12 @@ import com.andres_sagadoc.androidmaster.R
 class TodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // enableEdgeToEdge()
-        enableDarkModeActionBar()
-        changeNameActionBar(getString(R.string.todo_app))
+        enableEdgeToEdge()
         setContentView(R.layout.activity_todo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-
-    private fun changeNameActionBar(name: String) {
-        supportActionBar?.title = name
-    }
-    private fun enableDarkModeActionBar() {
-        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-        when (currentNightMode) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                // Modo oscuro
-                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.background_app)))
-            }
         }
     }
 }
