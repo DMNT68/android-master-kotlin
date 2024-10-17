b6aa67e0589edee69ad553a841f6ce0a27896291
Andrés Salgado <andressalgado41@gmail.com>
2024-10-17 18:17:04 -0500

    Init rvCategories

--- app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TodoActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TodoActivity.kt
@@ -1,15 +1,26 @@
 package com.andres_sagadoc.androidmaster.TODO
 
-import android.content.res.Configuration
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.andres_sagadoc.androidmaster.R
 
 class TodoActivity : AppCompatActivity() {
+
+    private val categories = listOf(
+        TaskCategory.Business,
+        TaskCategory.Personal,
+        TaskCategory.Work,
+        TaskCategory.Other
+    )
+
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +30,19 @@ class TodoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponent()
+        initUI()
+    }
+
+    private fun initUI() {
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
+    }
+
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
     }
 }
