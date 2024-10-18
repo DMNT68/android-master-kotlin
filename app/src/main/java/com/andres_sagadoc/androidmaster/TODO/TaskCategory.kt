21f5ee78e876f3e07773c85bcb1cf3d9d99442e2
Andrés Salgado <andressalgado41@gmail.com>
2024-10-18 18:36:48 -0500

    Add a property

--- app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TaskCategory.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TaskCategory.kt
@@ -1,6 +1,6 @@
 package com.andres_sagadoc.androidmaster.TODO
 
-sealed class TaskCategory {
+sealed class TaskCategory(var isSelected: Boolean = true) {
     object Business : TaskCategory()
     object Personal : TaskCategory()
     object Work : TaskCategory()
