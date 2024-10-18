62fe4a20c072f69a36457cded27ee6da931dfdca
Andrés Salgado <andressalgado41@gmail.com>
2024-10-18 13:04:30 -0500

    Update TodoActivity.kt

--- app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TodoActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/TODO/TodoActivity.kt
@@ -8,18 +8,35 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andres_sagadoc.androidmaster.R
+import com.andres_sagadoc.androidmaster.TODO.TaskCategory.*
 
 class TodoActivity : AppCompatActivity() {
 
     private val categories = listOf(
-        TaskCategory.Business,
-        TaskCategory.Personal,
-        TaskCategory.Work,
-        TaskCategory.Other
+        Business,
+        Personal,
+        Work,
+        Other
+    )
+
+    private val tasks = listOf<Task>(
+        Task("Personal", Personal, false),
+        Task("Trabajo 8 a 5", Work, false),
+        Task("Suscribirse al canal", Personal, false),
+        Task("Vender sistema", Business, false),
+        Task("Estudiar ingles", Personal, false),
+        Task("Terminar el listado", Work, false),
+        Task("Terminar Rachet and clank", Personal, false),
+        Task("Reunirse con el cliente", Business, false),
+        Task("Terminal Ralomtex", Work, false),
+        Task("Entregar movilitix", Work, false),
     )
 
     private lateinit var rvCategories: RecyclerView
+    private lateinit var rvTasks: RecyclerView
+
     private lateinit var categoriesAdapter: CategoriesAdapter
+    private lateinit var tasksAdapter: TasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +57,15 @@ class TodoActivity : AppCompatActivity() {
         rvCategories.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvTasks.adapter = tasksAdapter
     }
 
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTask)
     }
 }
