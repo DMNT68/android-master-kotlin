1c7fd0dd8af938c8bc79b1f3739569f58b4420b7
Andrés Salgado <andressalgado41@gmail.com>
2024-10-22 17:49:42 -0500

    Add service superHeroDetail

--- app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/ApiService.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/ApiService.kt
@@ -8,4 +8,7 @@ interface ApiService {
     @GET("/api/e887b9560ccc9a3a5ca38ee237a8d201/search/{name}")
     suspend fun getSuperHeros(@Path("name") superHeroName: String): Response<SuperHeroDataResponse>
 
+    @GET("/api/e887b9560ccc9a3a5ca38ee237a8d201/{id}")
+    suspend fun getSuperHeroDetail(@Path("id")id: String): Response<SuperHeroDetailResponse>
+
 }
