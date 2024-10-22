b47b07908bbb9c00f1dfeb4466b542ba0ff132f1
Andrés Salgado <andressalgado41@gmail.com>
2024-10-22 16:21:44 -0500

    Add SuperHeroAdapter.kt

---
+++ app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/SuperHeroAdapter.kt
@@ -0,0 +1,27 @@
+package com.andres_sagadoc.androidmaster.superheroapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.andres_sagadoc.androidmaster.R
+
+class SuperHeroAdapter(var superHeroList: List<SuperHeroResponse> = emptyList()) :
+    RecyclerView.Adapter<SuperHeroViewHolder>() {
+
+    fun updateList(superHeroList: List<SuperHeroResponse>) {
+        this.superHeroList = superHeroList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        return SuperHeroViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = superHeroList.size
+
+    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
+        holder.bind(superHeroList[position])
+    }
+}
