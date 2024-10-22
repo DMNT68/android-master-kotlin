58607cb78d4dbf9ade517de034b7f6baf7986c76
Andrés Salgado <andressalgado41@gmail.com>
2024-10-22 09:52:47 -0500

    Add SuperHeroList.kt

---
+++ app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/SuperHeroList.kt
@@ -0,0 +1,69 @@
+package com.andres_sagadoc.androidmaster.superheroapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.andres_sagadoc.androidmaster.R
+import com.andres_sagadoc.androidmaster.databinding.ActivitySuperHeroListBinding
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class SuperHeroList : AppCompatActivity() {
+    private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        retrofit = getRetrofit()
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.svHero.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean = false
+        })
+    }
+
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val myResponse: Response<SuperHeroDataResponse> =
+                retrofit.create(ApiService::class.java).getSuperHeros(query)
+            if (myResponse.isSuccessful) {
+                Log.i("andres", "Funciona :)")
+            } else {
+                Log.i("andres", "No funciona :(")
+            }
+        }
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://www.superheroapi.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
