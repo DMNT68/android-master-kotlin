26b38e74cf81497092c3a49964ca3c79f59f641c
Andrés Salgado <andressalgado41@gmail.com>
2024-10-22 16:18:58 -0500

    Add data needs

--- app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/SuperHeroDataResponse.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/SuperHeroDataResponse.kt
@@ -2,4 +2,67 @@ package com.andres_sagadoc.androidmaster.superheroapp
 
 import com.google.gson.annotations.SerializedName
 
-data class SuperHeroDataResponse(@SerializedName("response") val response: String)
+data class SuperHeroDataResponse(
+    @SerializedName("response") val response: String,
+    @SerializedName("results") val superHeros: List<SuperHeroResponse>
+)
+
+data class SuperHeroResponse(
+    @SerializedName("id") val superHeroId: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("image") val image: Image
+
+//    val powerstats: Powerstats,
+//    val biography: Biography,
+//    val appearance: Appearance,
+//    val work: Work,
+//    val connections: Connections,
+)
+
+data class Image(
+    @SerializedName("url") val url: String
+)
+
+/*
+data class Appearance(
+    val gender: String,
+    val race: String,
+    val height: List<String>,
+    val weight: List<String>,
+    val eyeColor: String,
+    val hairColor: String
+)
+
+data class Biography(
+    val fullName: String,
+    val alterEgos: String,
+    val aliases: List<String>,
+    val placeOfBirth: String,
+    val firstAppearance: String,
+    val publisher: String,
+    val alignment: String
+)
+
+data class Connections(
+    val groupAffiliation: String,
+    val relatives: String
+)
+
+data class Powerstats(
+    val intelligence: String,
+    val strength: String,
+    val speed: String,
+    val durability: String,
+    val power: String,
+    val combat: String
+)
+
+data class Work(
+    val occupation: String,
+    val base: String
+)
+*/
+
+
+
+
