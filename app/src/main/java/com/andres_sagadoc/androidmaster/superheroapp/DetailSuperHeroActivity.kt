93eb9b9d3d9e036745b05b19f19d0448cb1ac5cb
Andrés Salgado <andressalgado41@gmail.com>
2024-10-23 17:11:36 -0500

    Add fun prepareStats and bindin fullname and publisher

--- app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/DetailSuperHeroActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/superheroapp/DetailSuperHeroActivity.kt
@@ -1,11 +1,10 @@
 package com.andres_sagadoc.androidmaster.superheroapp
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.andres_sagadoc.androidmaster.R
 import com.andres_sagadoc.androidmaster.databinding.ActivityDetailSuperHeroBinding
 import com.andres_sagadoc.androidmaster.superheroapp.SuperHeroList.Companion.ID_SUPER_HERO
 import com.squareup.picasso.Picasso
@@ -15,6 +14,7 @@ import kotlinx.coroutines.launch
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.roundToInt
 
 class DetailSuperHeroActivity : AppCompatActivity() {
 
@@ -45,7 +45,30 @@ class DetailSuperHeroActivity : AppCompatActivity() {
     private fun createUI(superHero: SuperHeroDetailResponse) {
         Picasso.get().load(superHero.image.url).into(binding.ivSuperHero)
         binding.tvSuperHeroName.text = superHero.name
+        prepareStats(superHero.powerstats)
+        binding.tvSuperHeroFullName.text = superHero.biography.fullName
+        binding.tvSuperHeroPublisher.text = superHero.biography.publisher
     }
+
+    private fun prepareStats(powerstats: Powerstats) {
+        updateHeight(binding.vIntelligence, powerstats.intelligence)
+        updateHeight(binding.vStrength, powerstats.strength)
+        updateHeight(binding.vSpeed, powerstats.speed)
+        updateHeight(binding.vDurability, powerstats.durability)
+        updateHeight(binding.vPower, powerstats.power)
+        updateHeight(binding.vCombat, powerstats.combat)
+    }
+
+
+    private fun updateHeight(view: View, stat: String) {
+        val params = view.layoutParams
+        params.height = pxToDp(stat.toFloat())
+        view.layoutParams = params
+    }
+
+    private fun pxToDp(px: Float): Int =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics)
+            .roundToInt()
 
     private fun getRetrofit(): Retrofit {
         return Retrofit
