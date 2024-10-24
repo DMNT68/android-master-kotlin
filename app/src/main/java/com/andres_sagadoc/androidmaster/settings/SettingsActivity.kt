dfecfd5ed62b706a1f83249cc509e2de11da32db
Andrés Salgado <andressalgado41@gmail.com>
2024-10-24 16:27:06 -0500

    Add DataStore and Flows

--- app/src/main/java/com/andres_sagadoc/androidmaster/settings/SettingsActivity.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/settings/SettingsActivity.kt
@@ -1,17 +1,43 @@
 package com.andres_sagadoc.androidmaster.settings
 
+import android.content.Context
 import android.content.res.Configuration
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.andres_sagadoc.androidmaster.R
 import com.andres_sagadoc.androidmaster.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+// creando base de datos
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
+
+    private var firsTime: Boolean = true
+
+    companion object {
+        const val VOLUME_LVL = "volume_lvl"
+        const val KEY_BLUETOOTH = "key_bluetooth"
+        const val KEY_VIBRATION = "key_vibration"
+        const val KEY_DARK_MODE = "key_dark_mode"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +52,71 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Recuperando datos
+            getSettings().filter { firsTime }.collect { settingsModel ->
+                if (settingsModel != null) {
+                    runOnUiThread {
+                        binding.rsVolumen.setValues(settingsModel.volume.toFloat())
+                        binding.switchBluetooth.isChecked = settingsModel.bluetooth
+                        binding.switchVibration.isChecked = settingsModel.vibration
+                        binding.switchDarMode.isChecked = settingsModel.darkMode
+                        firsTime = !firsTime
+                    }
+                }
+            }
+        }
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.rsVolumen.addOnChangeListener { _, value, _ ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
+        }
+
+        binding.switchBluetooth.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_BLUETOOTH, value)
+            }
+        }
+        binding.switchVibration.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_VIBRATION, value)
+            }
+        }
+        binding.switchDarMode.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_DARK_MODE, value)
+            }
+        }
+    }
+
+    // Guardando datos
+    private suspend fun saveVolume(value: Int) {
+        dataStore.edit { preferences ->
+            preferences[intPreferencesKey(VOLUME_LVL)] = value
+        }
+    }
+
+    // Guardando datos
+    private suspend fun saveOptions(key: String, value: Boolean) {
+        dataStore.edit { preference -> preference[booleanPreferencesKey(key)] = value }
+    }
+
+    // Recuperando datos
+    private fun getSettings(): Flow<SettingsModel?> {
+        return dataStore.data.map { preferences ->
+            SettingsModel(
+                volume = preferences[intPreferencesKey(VOLUME_LVL)] ?: 50,
+                bluetooth = preferences[booleanPreferencesKey(KEY_BLUETOOTH)] ?: false,
+                darkMode = preferences[booleanPreferencesKey(KEY_DARK_MODE)] ?: false,
+                vibration = preferences[booleanPreferencesKey(KEY_VIBRATION)] ?: true,
+
+                )
         }
     }
 
