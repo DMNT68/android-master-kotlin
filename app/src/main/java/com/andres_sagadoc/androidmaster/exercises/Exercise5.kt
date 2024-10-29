6aef6694c42b06ee100e13736ce2393cce8e88ef
Andrés Salgado <andressalgado41@gmail.com>
2024-10-29 10:37:08 -0500

    Fix Exercise5.kt

--- app/src/main/java/com/andres_sagadoc/androidmaster/exercises/Exercise5.kt
+++ app/src/main/java/com/andres_sagadoc/androidmaster/exercises/Exercise5.kt
@@ -25,17 +25,22 @@ package com.andres_sagadoc.androidmaster.exercises
 fun main() {
     val amanda = Person("Amanda", 33, "play tennis", null)
     val atiqah = Person("Atiqah", 28, "climb", amanda)
+    val david = Person("Atiqah", 28, null, null)
 
     amanda.showProfile()
     atiqah.showProfile()
+    david.showProfile()
 }
 
 class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
     fun showProfile() {
-        val referrer =
-            if (referrer == null) " Doesn't have a referrer" else "Has a referrer named ${referrer.name}, who likes to ${referrer.hobby}"
+        var msg = ""
+        msg = if (hobby == null) "" else "Likes to $hobby. "
+        msg += if (referrer == null) "Doesn't have a referrer" else "Has a referrer named ${referrer.name}, who likes to ${referrer.hobby}"
+
         println("Name: $name")
         println("Age: $age")
-        println("Like to $hobby, $referrer \n")
+        println(msg)
+
     }
 }
